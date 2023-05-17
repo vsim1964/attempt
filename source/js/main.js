acf211070fee2088371f149ceb7bbd7c691df685
setkanet <128897001+setkanet@users.noreply.github.com>
2023-05-17 13:28:37 +0300

    +5

--- source/js/main.js
+++ source/js/main.js
@@ -1,4 +1,3 @@
-/* в этот файл добавляет скрипты*/
 import './menu.js';
 import './slider.js';
 import './map.js';
--- source/js/map.js
+++ source/js/map.js
@@ -1,14 +1,14 @@
-const MAP_ZOOM = 19;
-const SIZE_PIN = {
+const ZOOM = 18;
+const SIZE = {
   WIDTH: 38,
   HEIGHT: 50
 };
-const IMAGE_PIN = 'img/location.png';
-const MAP_COORDINATES = {
+
+const MAP = {
   LAT: 59.9684046045774,
   LNG: 30.31753421574238
 };
-const PIN_COORDINATES = {
+const PIN = {
   LAT: 59.96834017002364,
   LNG: 30.317673696510635
 };
@@ -19,21 +19,20 @@ mapSelector.classList.remove("map-nojs");
 const map = L.map('map')
   .on('load', () => { })
   .setView({
-    lat: MAP_COORDINATES.LAT,
-    lng: MAP_COORDINATES.LNG
-  },
-    MAP_ZOOM);
+    lat: MAP.LAT,
+    lng: MAP.LNG
+  }, ZOOM);
 
 
 const mapPinIcon = L.icon({
-  iconUrl: IMAGE_PIN,
-  iconSize: [SIZE_PIN.WIDTH, SIZE_PIN.HEIGHT],
-  iconAnchor: [SIZE_PIN.WIDTH / 2, SIZE_PIN.HEIGHT],
+  iconUrl: 'img/location.png',
+  iconSize: [SIZE.WIDTH, SIZE.HEIGHT],
+  iconAnchor: [SIZE.WIDTH / 2, SIZE.HEIGHT],
 });
 
 const mapPinMarker = L.marker({
-  lat: PIN_COORDINATES.LAT,
-  lng: PIN_COORDINATES.LNG
+  lat: PIN.LAT,
+  lng: PIN.LNG
 }, {
   draggable: false,
   icon: mapPinIcon,
