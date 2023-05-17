a5b1278ac0b81c47e8e478e84ca13503a8408250
setkanet <128897001+setkanet@users.noreply.github.com>
2023-05-17 12:53:55 +0300

    +4

--- source/js/slider.js
+++ source/js/slider.js
@@ -1,14 +1,14 @@
 const swiper = new Swiper('.slider', {
   loop: true,
-  // autoplay: {
-  //   delay: 5000,
-  // },
-  // speed: 400,
-  // spaceBetween: 30,
-  // effect: 'fade',
-  // fadeEffect: {
-  //   crossFade: true
-  // },
+  autoplay: {
+    delay: 5000,
+  },
+  speed: 400,
+  spaceBetween: 30,
+  effect: 'fade',
+  fadeEffect: {
+    crossFade: true
+  },
   pagination: {
     el: '.slider__pagination',
     clickable: true,
